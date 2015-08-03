8d6ea413276a6e5799e871ed618982d806bc5986
peters313 <ericpeters@Erics-MacBook-Pro.local>
2015-08-03 08:39:37 -0500

    adding dots and arrows

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -13,28 +13,68 @@ $(document).ready(function (){
                 createStudent.append('<p>Shout-out: ' + this.thanks + '</p>');
                 createStudent.append("<div class='footer'><button class='prev'>Previous</button><button class='next'>Next</button></div>");
             });
-            $('.nextCohortMember:first').addClass("nextSlide");
-
-            $('body').on('click', '.next', function(){
-                var currentSlide = $('.nextSlide');
+        //    $('.nextCohortMember:first').addClass(".nextSlide");
+        //
+        //    $('body').on('click', '.next', function(){
+        //        var currentSlide = $('.nextSlide');
+        //        var nextSlide = currentSlide.next();
+        //        if(nextSlide.length == 0){
+        //            nextSlide = $('.nextCohortMember:first');
+        //        }
+        //        currentSlide.removeClass(".nextSlide");
+        //        nextSlide.addClass(".nextSlide");
+        //
+        //    });
+        //
+        //    $("body").on("click", ".prev", function(){
+        //        var currentStudent = $(".nextSlide");
+        //        var prevStudent = currentStudent.prev();
+        //        if ( prevStudent.length == 0) {
+        //            prevStudent = $(".nextCohortMember:last");
+        //        }
+        //        currentStudent.removeClass(".nextSlide");
+        //        prevStudent.addClass(".nextSlide");
+        //    });
+        //}
+            $('.arrow-next').click(function() {
+                var currentSlide = $('.active-slide');
                 var nextSlide = currentSlide.next();
-                if(nextSlide.length == 0){
-                    nextSlide = $('.nextCohortMember:first');
-                }
-                currentSlide.removeClass("nextSlide");
-                nextSlide.addClass("nextSlide");
 
+                var currentDot = $('.active-dot');
+                var nextDot = currentDot.next();
+
+                if(nextSlide.length === 0) {
+                    nextSlide = $('.slide').first();
+                    nextDot = $('.dot').first();
+                }
+
+                currentSlide.fadeOut(600).removeClass('active-slide');
+                nextSlide.fadeIn(600).addClass('active-slide');
+
+                currentDot.removeClass('active-dot');
+                nextDot.addClass('active-dot');
             });
 
-            $("body").on("click", ".prev", function(){
-                var currentStudent = $(".nextSlide");
-                var prevStudent = currentStudent.prev();
-                if ( prevStudent.length == 0) {
-                    prevStudent = $(".nextCohortMember:last");
+
+            $('.arrow-prev').click(function() {
+                var currentSlide = $('.active-slide');
+                var prevSlide = currentSlide.prev();
+
+                var currentDot = $('.active-dot');
+                var prevDot = currentDot.prev();
+
+                if(prevSlide.length === 0) {
+                    prevSlide = $('.slide').last();
+                    prevDot = $('.dot').last();
                 }
-                currentStudent.removeClass("nextSlide");
-                prevStudent.addClass("nextSlide");
+
+                currentSlide.fadeOut(600).removeClass('active-slide');
+                prevSlide.fadeIn(600).addClass('active-slide');
+
+                currentDot.removeClass('active-dot');
+                prevDot.addClass('active-dot');
             });
+
         }
     });
 });
